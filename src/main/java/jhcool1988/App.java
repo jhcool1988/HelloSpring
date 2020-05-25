3ecbdebeac5ca5fa5a8a02c6fa39712242deaa7d
jhcool1988 <jhcool1988@gmail.com>
2020-05-25 15:03:33 +0900

    Initial commit

---
+++ src/main/java/jhcool1988/App.java
@@ -0,0 +1,13 @@
+package jhcool1988;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
